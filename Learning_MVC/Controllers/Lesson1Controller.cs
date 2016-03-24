743870c86eacd22e8fd57de163d26c11c24d9c68
tandisunarto <Spartan1170705>
2016-03-24 13:27:52 -0400

    JQuery Promises

---
+++ Learning_MVC/Controllers/Lesson1Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learning_MVC.Controllers
+{
+    public class Lesson1Controller : Controller
+    {
+        // GET: Lesson1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult JQueryPromises()
+        {
+            return View();
+        }
+
+        public ActionResult GetMovies()
+        {
+            var movie = new List<object>();
+
+            movie.Add(new { Title = "Ghostbusters", Genre = "Comedy", Year = 1984 });
+            movie.Add(new { Title = "Gone with Wind", Genre = "Drama", Year = 1939 });
+            movie.Add(new { Title = "Star Wars", Genre = "Science Fiction", Year = 1977 });
+
+            return Json(movie, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetMusic()
+        {
+            var music = new List<object>();
+
+            music.Add(new { Title = "Dystopia", Genre = "Heavy Metal", Year = 2016 });
+            music.Add(new { Title = "Invisible Touch", Genre = "Pop", Year = 1986 });
+            music.Add(new { Title = "Pure At Heart", Genre = "Instrumental", Year = 2013 });
+
+            return Json(music, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Learning_MVC/Scripts/PageScripts/JQueryPromises.js
@@ -0,0 +1,62 @@
+﻿$(function() {
+    $('#GetData').click(function () {
+        var GetMovies = $.getJSON(getMoviesURL);
+        var GetMusic = $.getJSON(getMusicURL);
+
+        // ** Ex 1 **
+        //GetMovies
+        //    .done(fnGetMoviesDone)
+        //    .fail(fnFail)
+        //    .always(fnAlways);
+
+        //GetMusic
+        //    .done(fnGetMusicDone)
+        //    .fail(fnFail)
+        //    .always(fnAlways);
+
+        // ** Ex 2 **
+        $.when(GetMovies, GetMusic).done(fnDone(GetMovies, GetMusic))
+    })
+    
+    function fnGetMoviesDone(response) {
+        if (response != null) {
+            $("#MovieList li").remove();
+            for (var i = 0; i < response.length; i++) {
+                $("#MovieList").append("<li>" + response[i].Title + " - " + response[i].Genre + " (" + response[i].Year + ")</li>")
+            }
+        }
+    }
+
+    function fnGetMusicDone(response) {
+        if (response != null) {
+            $("#MusicList li").remove();
+            for (var i = 0; i < response.length; i++) {
+                $("#MusicList").append("<li>" + response[i].Title + " - " + response[i].Genre + " (" + response[i].Year + ")</li>")
+            }
+        }
+    }
+    
+    function fnDone(GetMovies, GetMusic)
+    {
+        GetMovies
+            .done(fnGetMoviesDone)
+            .fail(fnFail)
+            .always(fnAlways);
+
+        GetMusic
+            .done(fnGetMusicDone)
+            .fail(fnFail)
+            .always(fnAlways);
+    }
+
+    function fnFail(response)
+    {
+        alert('fail');
+    }
+    
+    function fnAlways(response)
+    {
+        $("#result").html(new Date($.now()) + " Finish");
+    }
+
+})
