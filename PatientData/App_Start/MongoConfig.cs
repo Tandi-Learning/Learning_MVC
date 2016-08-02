04cf6f98a7306f31cf04f16ba31877b00985e97f
tandisunarto <tandi.sunarto@hotmail.com>
2016-08-02 18:05:51 -0400

    Initial Checkin for WebApi 2 and MongoDB

---
+++ PatientData/App_Start/MongoConfig.cs
@@ -0,0 +1,52 @@
+﻿using PatientData.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver;
+using PatientData.Models;
+
+namespace PatientData.App_Start
+{
+    public static class MongoConfig
+    {
+        public static void Seed()
+        {
+            var patients = PatientDB.Open();
+
+            if (!patients.AsQueryable().Any(p => p.Name == "Tandi"))
+            {
+                var data = new List<Patient>
+                {
+                    new Patient
+                    {
+                        Name = "John",
+                        Ailments = new List<Ailment> { new Ailment { Name = "John Ailment 1" }, new Ailment { Name = "John Ailment 2" } },
+                        Medications = new List<Medication> { new Medication { Name = "John Medication 1" }, new Medication { Name = "John Medication 2" } },
+                    },
+                    new Patient
+                    {
+                        Name = "Paul",
+                        Ailments = new List<Ailment> { new Ailment { Name = "Paul Ailment 1" }, new Ailment { Name = "Paul Ailment 2" } },
+                        Medications = new List<Medication> { new Medication { Name = "Paul Medication 1" }, new Medication { Name = "Paul Medication 2" } },
+                    },
+                    new Patient
+                    {
+                        Name = "George",
+                        Ailments = new List<Ailment> { new Ailment { Name = "George Ailment 1" }, new Ailment { Name = "George Ailment 2" } },
+                        Medications = new List<Medication> { new Medication { Name = "George Medication 1" }, new Medication { Name = "George Medication 2" } },
+                    },
+                    new Patient
+                    {
+                        Name = "Ringo",
+                        Ailments = new List<Ailment> { new Ailment { Name = "Ringo Ailment 1" }, new Ailment { Name = "Ringo Ailment 2" } },
+                        Medications = new List<Medication> { new Medication { Name = "Ringo Medication 1" }, new Medication { Name = "Ringo Medication 2" } },
+                    },
+                };
+
+                patients.InsertManyAsync(data);
+            }
+        }
+    }
+}
---
+++ PatientData/Data/PatientDB.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using PatientData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PatientData.Data
+{
+    public static class PatientDB
+    {
+        public static IMongoCollection<Patient> Open()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var db = client.GetDatabase("PatientDB");
+            return db.GetCollection<Patient>("Patients");
+        }
+    }
+}
