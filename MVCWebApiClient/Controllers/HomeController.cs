ac01fd727c13d25a5233942558555e1e208fd2ca
Tandi Sunarto <tandi.sunarto@hotmail.com>
2016-10-31 15:36:43 -0400

    Add GET logic for HttpClient

--- MVCWebApiClient/Controllers/HomeController.cs
+++ MVCWebApiClient/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +29,29 @@ namespace MVCWebApiClient.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult UseHttpClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("aplication/json"));
+
+            HttpResponseMessage response = client.GetAsync(new Uri("http://localhost:55467/api/Product")).Result;
+            string content = response.Content.ReadAsStringAsync().Result;
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(content);
+
+            return Json(new { ProductsResult = products }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult UseWebClient()
+        {
+            return Json(new { value = "UseWebClient" }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult UseHttpWebRequest()
+        {
+            return Json(new { value = "UseHttpWebRequest" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ MVCWebApiClient/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebApiClient
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string CategoryName { get; set; }
+        public int Price { get; set; }
+    }
+
+}
