eb5cc50cfd4c097c9566c76f6d526c1d130ede2b
tandisunarto <tandi.sunarto@hotmail.com>
2016-11-08 00:14:27 -0500

    Use IHttpActionResult

--- MVCWebApi/Controllers/Product2Controller.cs
+++ MVCWebApi/Controllers/Product2Controller.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace MVCWebApi.Controllers
@@ -37,6 +40,61 @@ namespace MVCWebApi.Controllers
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
+        }
+    }
+
+    public class Product3Controller : ApiController
+    {
+        // GET api/<controller>
+        public IHttpActionResult Get()
+        {
+            IEnumerable<Product> products = Products.instance.ProductList;
+            //            return Ok<IEnumerable<Product>>(products);
+            return new ProductsResult(products, Request);
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class ProductsResult : IHttpActionResult
+    {
+        private IEnumerable<Product> _products;
+        private HttpRequestMessage _request;
+
+        public ProductsResult(IEnumerable<Product> products, HttpRequestMessage request)
+        {
+            _products = products;
+            _request = request;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage
+            {
+                Content = new ObjectContent<IEnumerable<Product>>(_products, new JsonMediaTypeFormatter()),
+                RequestMessage = _request
+            };
+
+            return Task.FromResult(response);
         }
     }
 }
