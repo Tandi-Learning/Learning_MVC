cb9eeb54ce01e52a3321a43690d16e4d5119d274
tandisunarto <tandi.sunarto@hotmail.com>
2016-11-07 21:28:05 -0500

    Add api controller that returns HttpResponseMessage

---
+++ MVCWebApi/Controllers/Product2Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCWebApi.Controllers
+{
+    public class Product2Controller : ApiController
+    {
+        // GET api/<controller>
+        public HttpResponseMessage Get()
+        {
+            IEnumerable<Product> products = Products.instance.ProductList;
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, products);
+            return response;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
