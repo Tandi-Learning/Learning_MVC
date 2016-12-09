e9d595b53a45b8abf88765aec8ae4ca5961ac653
Tandi Sunarto <tandi.sunarto@hotmail.com>
2016-12-09 13:28:31 -0500

    added web api CORS and ajax .post call

--- MVCWebApiClient/Scripts/PageScripts/UseHttpClient.js
+++ MVCWebApiClient/Scripts/PageScripts/UseHttpClient.js
@@ -1,3 +1,14 @@
 ﻿$(document).ready(function () {
-
+    $('#createButton').on("click", function () {
+        $.post('http://localhost:55467/api/Product',
+            {
+                CategoryName: $('#CategoryName').val(),
+                Name: $('#Name').val(),
+                Price: $('Price').val(),
+            })
+        .success(function (data) {            
+        })
+        .fail(function (jqXHR, status, error) {
+        })
+    })
 });
